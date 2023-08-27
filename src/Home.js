c8425d86dc25a0ca31c989da34ba768e122ea36c
Kongo67 <kongo@aucegypt.edu>
2023-08-27 12:06:13 +0300

    wallpaper

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.css';
-import AppNavbar from './AppNavbar';
 import { Link } from 'react-router-dom';
 import { Button, Container } from 'reactstrap';
 
 const Home = () => {
+  const backgroundImageUrl = 'url("https://i4.hurimg.com/i/hurriyet/75/750x422/5e1dacfc0f2544125445cb81.jpg")'; // Replace with your direct image URL
+
   return (
-     <div style={{ alignItems : "center" }} >
-      <AppNavbar/>
-      <Container fluid>
-        <Button style={{fontSize : 40}} tag={Link} to="/training" >Manage Trainings</Button>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        backgroundImage: backgroundImageUrl,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Container>
+        <Button style={{ fontSize: 40 }} tag={Link} to="/training">Manage Trainings</Button>
       </Container>
     </div>
   );
